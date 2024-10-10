222e8149164c6268954202ac392cd1d162b0cac0
harireddy0711 <harireddy9915@gmail.com.com>
2024-10-10 11:07:36 +0100

    corrected the script

--- message_gen.js
+++ message_gen.js
@@ -2,13 +2,13 @@ const string1 = ["You", "Your Mind", "Your Heart", "Your Brain"];
 const string2 = ["is capable of", "has the power to", "can achieve", "will bring"];
 const string3 = ["Good thins", "Priceless Growth", "a colorfull Future", "Success"];
 
-function generateMeaasge() {
+function generateMessage() {
     const randomString1 = string1[Math.floor(Math.random() * string1.length)];
     const randomString2 = string2[Math.floor(Math.random() * string2.length)];
     const randomString3 = string3[Math.floor(Math.random() * string3.length)];
     
     const message = `${randomString1} ${randomString2} ${randomString3}`;
+    console.log(message);
 }
-console.log(message);
 
-generateMeaasge();
+generateMessage();
